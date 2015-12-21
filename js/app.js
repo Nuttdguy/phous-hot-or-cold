88cf6958290c1337fcecaaf23e0bbac9b01b7e56
Nuttdguy <pygnasak@gmail.com>
2015-12-20 23:53:50 -0600

    refactor to use functions that contain functions. For testing understanding

--- js/app.js
+++ js/app.js
@@ -1,98 +1,125 @@
+var secret = 0; // initializing secret variable within global object -- not very secret
+var count = 0;
+var alreadyGuessed = [];
 
 $(document).ready(function(){
+
+	newGame(); //  loaded into global window object - event triggered by "new game" button
+	currentGuess(); // loaded into global window object - event triggered by click
+	getSecretNumber(); // invokes the function to generate a secret number
 	
-	startGame();
 
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+/*--- Display information modal box ---*/
+	$(".what").click(function(){
+		$(".overlay").fadeIn(1000);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
 });
 
 
+function getSecretNumber() {  // Generates a secret number
+	
+	this.secret = Math.floor((Math.random() * 100));
+	console.log(secret);
+	
+}
 
-function startGame() {
-	
-	var secret;
-	function activeSecretNumber() {
-		secret = Math.floor((Math.random() * 100));
-	}
-	
-	activeSecretNumber();
-	
-	//--------------------------------------------
-	
-	var count = 0;
+function currentGuess() {  // Invokes on click of guess button
+
 	$('#guessButton').on('click', function(event) {
 		event.preventDefault();
-		
-		var currentGuess = parseInt($('#userGuess').val());		
-		$('#guessList').append('<li>' + currentGuess + '</li>');
+
+		var guess = parseInt($('#userGuess').val());		
+		$('#guessList').append('<li>' + guess + '</li>');
 		$('#userGuess').val('');
-		console.log(secret + ' -- secret number ');
-		console.log(currentGuess + ' -- current guess ');
-			
-		var howClose; 
-		if (secret > currentGuess) {
-			howClose = (secret - currentGuess);
-		} else {
-			howClose = (currentGuess - secret);
-		}
 		
-		if (currentGuess === secret) {
-			$('#feedback').text('Game Over!');
-			activeSecretNumber();
-			$('#guessList li').remove();
-			count = -1;
-			$('#count').text(count);
-		} else if (howClose <= 5) {
-			$('#feedback').text('Stop! You\'re hair is on fire!')
-		} else if (howClose <= 10) {
-			$('#feedback').text('Look! You\'re car is on fire!')
-//			console.log(howClose + ' -- You\'re within 10');
-		} else if (howClose <= 20) {
-			$('#feedback').text('Hey you, over there! Too close!')
-//			console.log(howClose + ' -- You\'re within 20'); 
-		} else if (howClose <= 30) {
-			$('#feedback').text('You\'re making me sweat!');
-//			console.log(howClose + ' -- You\'re within 30');
-		} else if (howClose <= 40) {
-			$('#feedback').text('And I thought you could read my mind!');
-//			console.log(howClose + ' -- You\'re within 40');
-		} else {
-			$('#feedback').text('Ain\'t never going to get it!');
-//			console.log(howClose + ' -- You\'re a mile away');
-		}
-		
-		if (!$.isNumeric(currentGuess)) {
-			$('#userGuess').val('');
-			$('#guessList li').last().remove();
-			count--;
-			$('#count').text(count);
-		} 
-		
-		count++;
-		$('#count').text(count);
-		
+		didUserGuess(guess);
+		isGuessNumeric(guess); // passes guess, validates user input and increments/decrements counter
+		howClose(guess); // passes value to howClose function
+
 	})
 	
-	function newGame() {
+}
+
+var didUserGuess = function(guessedNumbers) {
+	
+	alreadyGuessed.push(guessedNumbers);
+	
+	for(i = 0; i < alreadyGuessed.length; i++) {
+		var checkGuessDuplicate = alreadyGuessed[i];
+	}
+	
+	console.log(alreadyGuessed);
+}
+
+var isGuessNumeric = function(num) {
+	
+	if (!$.isNumeric(num)) { // Check if user guesss is a number
+		$('#userGuess').val('');  // resets the input box value
+		$('#guessList li').last().remove(); // removes the link last generated
+		return;
+	} 
+		
+		count++; // increments the global count variable
+		$('#count').text(count); // updates the count text
+}
+
+
+var howClose = function(userGuess) {
+	
+//	console.log(secret);
+//	console.log(userGuess + "  in howClose function");
+	
+	var howCloseIsGuess; // declares the variable for resulting value
+	if (secret > userGuess) {
+		howCloseIsGuess = (secret - userGuess); // 
+	} else {
+		howCloseIsGuess = (userGuess - secret);
+	}	
+	
+	// passes users guess and calculated difference to checkUserGuess function
+	checkUserGuess(userGuess, howCloseIsGuess);
+	
+}
+
+function checkUserGuess(userGuess, isGuessCorrect) {
+//	console.log( isGuessCorrect + "  in checkUserGuess function")
+	
+	if (userGuess === secret) { // checks if userGuess is correct
+		$('#feedback').text('You Win! Game Over!');
+		getSecretNumber();  // generates new secret number
+		$('#guessList li').remove(); // removes all guesses
+		count = 0; // return count to 0
+		$('#count').text(count); // return count text to 0
+	} else if (isGuessCorrect <= 5) {
+		$('#feedback').text('Stop! You\'re hair is on fire!')
+	} else if (isGuessCorrect <= 10) {
+		$('#feedback').text('Look! You\'re car is on fire!')
+	} else if (isGuessCorrect <= 20) {
+		$('#feedback').text('Hey you, over there! Too close!')
+	} else if (isGuessCorrect <= 30) {
+		$('#feedback').text('You\'re making me sweat!');
+	} else if (isGuessCorrect <= 40) {
+		$('#feedback').text('And I thought you could read my mind!');
+	} else {
+		$('#feedback').text('Ain\'t never going to get it!');
+	}	
+}
+
+function newGame() { // reset game
+	
 	$('.new').on('click', function(e) {
-		activeSecretNumber();
+		getSecretNumber();
 		count = 0;
 		$('#count').text(count);
 		$('#guessList li').remove();
-		console.log(secret);
-		})
-	}
-	
+	})	
 }
 
 
